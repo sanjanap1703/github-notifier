6f11b66020fdd0fdc792574cc3e6bd4dc4c52699
Sanjana Bipin Pawar <psanjana077@gmail.com>
2023-09-13 16:35:30 +0530

    .\main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import os
 from twilio.rest import Client
 
-account_sid = os.environ['sid']
-auth_token = os.environ['token']
-to_whatsapp_no = os.environ['whatsapp_no']
+account_sid = os.environ['account_sid']
+auth_token = os.environ['auth_token']
+to_whatsapp_no = os.environ['to_whatsapp_no']
 from_whatsapp_no = os.environ['from_whatsapp_no']
 
-client = Client(sid,token)
+client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='Code Pushed in main branch of https://github.com/sanjanap1703/github-notifier',
+                              body='Code Pushed in main branch of  https://github.com/sanjanap1703/github-notifier',
                               from_='whatsapp:'+from_whatsapp_no,
-                              to='whatsapp:'+whatsapp_no
+                              to='whatsapp:'+to_whatsapp_no
                           )
 
 print("Message ID:",message.sid)
